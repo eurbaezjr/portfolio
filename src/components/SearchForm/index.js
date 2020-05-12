e7cbf17367ece2a2e6320b5b0a10c908d7b08d01
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-05-12 02:15:39 -0700

    added icon

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./style.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function SearchForm(props) {
   return (
     <form className="justify-content-center">
       <div className="form-group">
+
+      <FontAwesomeIcon icon={faSearch} id="search-icon"/>
         <input
           onChange={props.handleInputChange}
           value={props.search}
           name="search"
           type="text"
           className="form-control"
-          placeholder="Search accross all projects"
+          placeholder="Search accross all my projects"
           id="search"
         />
+   
       </div>
     </form>
   );
