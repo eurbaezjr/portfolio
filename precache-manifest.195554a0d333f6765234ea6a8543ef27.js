73aca07b8403449f1ee30c5d301fc70847816b90
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-05-13 00:37:16 -0700

    Updates

---
+++ precache-manifest.195554a0d333f6765234ea6a8543ef27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c30f30de81bcf734ae7612d1b4a10f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "805c3674bcd8d605e810",
+    "url": "/portfolio/static/css/2.f681e662.chunk.css"
+  },
+  {
+    "revision": "fbec3186725d1e7feab7",
+    "url": "/portfolio/static/css/main.ebd8c140.chunk.css"
+  },
+  {
+    "revision": "805c3674bcd8d605e810",
+    "url": "/portfolio/static/js/2.b5e7fb29.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/portfolio/static/js/2.b5e7fb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbec3186725d1e7feab7",
+    "url": "/portfolio/static/js/main.e01f3c6a.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "522451fcbfe64d668a3af0484ff7330e",
+    "url": "/portfolio/static/media/profile-generator-demo.522451fc.gif"
+  }
+]);
