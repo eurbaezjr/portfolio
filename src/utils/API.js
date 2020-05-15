732a6c63efb136431fd84aa4ffa2e4b7d1c37046
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-05-14 23:22:56 -0700

    test

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,13 +4,21 @@ import axios from "axios";
 
 export default {
 
+
   getStarredRepos: function(gitHubUserName) {
-    return axios.get("https://api.github.com/users/" + gitHubUserName + "/starred"
+    return axios.get("https://api.github.com/users/" + gitHubUserName + "/starred", 
+    headers = {
+      'Content-type': 'text/plain',
+      "Access-Control-Allow-Origin": "https://eurbaezjr.github.io/portfolio/projects"
+    }
     );
   },
   getReposContent: function(fullRepoName) {
-    return axios.get("https://api.github.com/repos/" + fullRepoName + "/contents"
+    return axios.get("https://api.github.com/repos/" + fullRepoName + "/contents",
+    headers = {
+      'Content-type': 'text/plain',
+      "Access-Control-Allow-Origin": "https://eurbaezjr.github.io/portfolio/projects"
+    }
     );
   }
 };
-
